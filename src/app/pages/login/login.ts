fde456e36f8f1ddeee51c886bcd03a9558c83603
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-08-02 16:34:58 -0300

    feat: adiciona tela de login e configura roteamento inicial

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,29 @@
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  private authService = inject(Auth);
+  private router = inject(Router);
+
+  username = signal('');
+  password = signal('');
+  erro = signal('');
+
+  entrar() {
+    this.authService.login(this.username(), this.password()).subscribe({
+      next: (resposta: any) => {
+        console.log('Login realizado', resposta);
+      },
+      error: () => {
+        this.erro.set('Usuário ou senha inválidos.');
+      },
+    });
+  }
+}
