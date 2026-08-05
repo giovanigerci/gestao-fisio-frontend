08ca3565fa7faffb16a62b1342136ffa24485e78
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-08-05 18:41:38 -0300

    feat: adiciona guard de rota para proteger acesso ao dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'dashboard', component: Dashboard },
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
 ];
